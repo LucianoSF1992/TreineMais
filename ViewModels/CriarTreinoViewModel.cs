d2a9bf98d18d26e84799858dd5dd326d09fc1041
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-02-25 15:58:00 -0300

    feat: adiciona propriedades de treino e exercício no CriarTreinoViewModel

--- ViewModels/CriarTreinoViewModel.cs
+++ ViewModels/CriarTreinoViewModel.cs
@@ -5,7 +5,22 @@ namespace TreineMais.ViewModels
     public class CriarTreinoViewModel
     {
         public int AlunoId { get; set; }
+
         public string NomeTreino { get; set; } = string.Empty;
+
+        public string DiaSemana { get; set; } = string.Empty;
+
+        public string NomeExercicio { get; set; } = string.Empty;
+
+        public string GrupoMuscular { get; set; } = string.Empty;
+
+        public int Series { get; set; }
+
+        public int Repeticoes { get; set; }
+
+        public string Descanso { get; set; } = string.Empty;
+
+        public string Observacoes { get; set; } = string.Empty;
 
         public List<ApplicationUser> Alunos { get; set; } = new();
     }
--- obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
+++ obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4e8f09d5f71267d842cae8e9f5c8b1eb84aa20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2a60579e530a771fe6781b0665c9258459d6a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
