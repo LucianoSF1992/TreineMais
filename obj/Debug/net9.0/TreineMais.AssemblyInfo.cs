19fffce1c7d549d4ceeb33c6f18560e1787b58bf
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-02-25 08:40:10 -0300

    chore: adicionando gitignore

--- obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
+++ obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03e76b5df41738c9948bd600c982d3a0b2acc0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
