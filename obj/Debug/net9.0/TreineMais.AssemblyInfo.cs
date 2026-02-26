55836f9779fb5d9a70e6c1db4536ee183d6ba4bb
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-02-26 06:59:03 -0300

    feat: implementa landing page inicial do TreineMais

--- obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
+++ obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2aa10fc895859005916060457dc191d0f26921ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ccf07d425c65894336fe993ed18acba6e7e632")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
