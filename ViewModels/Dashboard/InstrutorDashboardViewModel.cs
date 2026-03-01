23061bd65a0b8424d84b56176f0fba6a4bd204c2
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-02-28 22:33:43 -0300

    feat: implementar dashboards por role com cards e tabelas

--- ViewModels/Dashboard/InstrutorDashboardViewModel.cs
+++ ViewModels/Dashboard/InstrutorDashboardViewModel.cs
@@ -4,10 +4,7 @@ namespace TreineMais.ViewModels.Dashboard
 {
     public class InstrutorDashboardViewModel
     {
-        public int TotalAlunos { get; set; }
         public int TotalTreinos { get; set; }
-
-        public List<ApplicationUser> Alunos { get; set; } = new();
         public List<Treino> MeusTreinos { get; set; } = new();
     }
 }
