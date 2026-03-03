fc52ac2155969eeba73e44912128adebbc86fb44
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-03-03 09:02:28 -0300

    feat(ui): melhorar responsividade do dashboard do instrutor

--- obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
+++ obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+566bd3969a083eb88abb8d697d027347aa6ba26b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f955063fef689a9af87dfbb4fb9ef1e285aeab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
