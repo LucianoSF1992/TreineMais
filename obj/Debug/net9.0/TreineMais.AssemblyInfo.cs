b75f8a695fbe9038bbbaaa3e8217b2dd1253cee2
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-03-04 06:26:06 -0300

    fix(layout): corrigir espaço abaixo do footer usando sticky footer

--- obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
+++ obj/Debug/net9.0/TreineMais.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc52ac2155969eeba73e44912128adebbc86fb44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a12c661a22765a4670aebd4fba2208ca256c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TreineMais")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
