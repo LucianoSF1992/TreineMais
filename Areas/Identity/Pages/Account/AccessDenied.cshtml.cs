6c7da096b39465151095f50e476aa581e35576ea
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-03-04 09:19:30 -0300

    feat: criar página AccessDenied com navegação para dashboard

---
+++ Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TreineMais.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
